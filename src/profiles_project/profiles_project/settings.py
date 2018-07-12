975a9ea3c8e304187d92e1a4106685907ab5f358
amirfeqhi <amirfeqhi@gmail.com>
2018-07-13 01:37:28 +0430

    Updated django settings file

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
