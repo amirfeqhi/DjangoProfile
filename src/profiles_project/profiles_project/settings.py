2102eac4aeae2c4274346cf4d97d664b1837086b
amirfeqhi <amirfeqhi@gmail.com>
2018-07-12 21:43:57 +0430

    Added some changes to settings file

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
@@ -120,5 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
