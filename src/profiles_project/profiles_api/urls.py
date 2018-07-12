346c23cdf3020806bcc8998fb1a310008a7d266c
amirfeqhi <amirfeqhi@gmail.com>
2018-07-12 20:17:24 +0430

    Commented some unnecessary codes

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,12 +6,12 @@ from . import views
 
 
 router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+# router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet, base_name='login')
 router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
-    url('^hello-view/', views.HelloApiView.as_view()),        
+#     url('^hello-view/', views.HelloApiView.as_view()),        
     url('', include(router.urls)),
 ]
