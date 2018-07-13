46529ad90e1685342fc8c96b26384fa2a77d50d2
amirfeqhi <amirfeqhi@gmail.com>
2018-07-13 12:32:28 +0430

    Configure django setting file for heroku usage

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +128,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
